c98c26e014b4d59b2209d674e95ce86b013c43db
Matt Lane <lane.matthew@gmail.com>
2021-06-25 10:13:20 -0700

    add error handling

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const categoryRoutes = require("./routes/categories");
+const { NotFoundError } = require("./utils/errors");
 
 const app = express();
 
@@ -8,6 +9,19 @@ app.use(morgan("tiny"));
 app.use(express.json());
 
 app.use("/categories", categoryRoutes);
+
+app.use(function (req, res, next) {
+  return next(new NotFoundError());
+});
+
+app.use(function (err, req, res, next) {
+  const status = err.status || 500;
+  const message = err.message;
+
+  return res.status(status).json({
+    error: { message, status }
+  });
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
